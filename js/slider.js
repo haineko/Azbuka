d8ed7721144083e27db5da58f3c3103e1f8ab036
Natalia Yorkina <yorkinan@gmail.com>
2015-10-02 18:37:15 +0300

    fix Alinas comments

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,18 @@
-!function(){var e=document.querySelectorAll("slideSix_arrow"),c=document.querySelectorAll('[name^="r-toggle"]');console.log(e,c);for(var t=0;t<e.length;t++)e[t].addEventListener("click",function(e){e.preventDefault();var t=document.querySelector(".slideSix_inner input:checked"),r=[].indexOf.call(c,t),l=c.length;switch(console.log("Работает"),this.dataset.direction){case"left":0==r?c[l-1].checked=!0:c[r-1].checked=!0;break;case"right":l-1==r?c[0].checked=!0:c[r+1].checked=!0}})}();
+! function() {
+  var e = document.querySelectorAll("slideSix_arrow"),
+    c = document.querySelectorAll('[name^="r-toggle"]');
+  console.log(e, c);
+  for (var t = 0; t < e.length; t++) e[t].addEventListener("click", function(e) {
+    e.preventDefault();
+    var t = document.querySelector(".slideSix_inner input:checked"),
+      r = [].indexOf.call(c, t),
+      l = c.length;
+    switch (this.dataset.direction) {
+      case "left":
+        0 == r ? c[l - 1].checked = !0 : c[r - 1].checked = !0;
+        break;
+      case "right":
+        l - 1 == r ? c[0].checked = !0 : c[r + 1].checked = !0
+    }
+  })
+}();
